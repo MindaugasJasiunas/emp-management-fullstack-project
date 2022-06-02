046d4ecf220528fead115ca423f4644fefa07f03
MindaugasJasiunas <mindaugas.jasiunas.it@gmail.com>
2022-06-02 10:12:09 +0300

    updated HttpResponse POJO to a Record. #7

--- backend/src/main/java/com/example/demo/HttpResponse.java
+++ backend/src/main/java/com/example/demo/HttpResponse.java
@@ -6,13 +6,4 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
 
-@AllArgsConstructor
-@Setter
-@Getter
-// response to send to user every time
-public class HttpResponse {
-    private int httpStatusCode;
-    private HttpStatus httpStatus;
-    private String reason;
-    private String message;
-}
+public record HttpResponse(int httpStatusCode, HttpStatus httpStatus, String reason, String message) {}
