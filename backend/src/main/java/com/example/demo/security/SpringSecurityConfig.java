3a90708e3523d78c87170b19e3e33cde32449d34
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-06-08 15:55:19 +0300

    Spring Security Config OLD WAY (WebSecurityConfigurerAdapter deprecated) #9. Closes #10

--- backend/src/main/java/com/example/demo/security/SpringSecurityConfig.java
+++ backend/src/main/java/com/example/demo/security/SpringSecurityConfig.java
@@ -1,46 +1,95 @@
 package com.example.demo.security;
 
+import com.example.demo.filter.JwtAccessDeniedHandler;
+import com.example.demo.filter.JwtAuthenticationEntryPoint;
+import com.example.demo.filter.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
-import static org.springframework.security.config.Customizer.withDefaults;
 
-// Updated Web Security configuration ('extends WebSecurityConfigurerAdapter' is deprecated)
+import java.util.Arrays;
+
 @Configuration
 @EnableWebSecurity
-//public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
-public class SpringSecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserServiceImpl userService;
+    private final JwtAuthorizationFilter jwtAuthFilter;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 
-//    protected void configure(HttpSecurity http) throws Exception {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeHttpRequests((authz) -> authz
-                        .antMatchers(HttpMethod.GET, "/api/v1/**").permitAll()
-                        .antMatchers(HttpMethod.PUT, "/api/v1/**").hasRole("SUPER_ADMIN")
-                        .antMatchers(HttpMethod.DELETE, "/api/v1/**").hasAuthority("canDeleteUsers")
-                        .anyRequest().authenticated()
-                )
-                .httpBasic(withDefaults());
-        return http.build();
+    public SpringSecurityConfig(UserServiceImpl userService, JwtAuthorizationFilter jwtAuthFilter, JwtAccessDeniedHandler jwtAccessDeniedHandler, JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint) {
+        this.userService = userService;
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.jwtAccessDeniedHandler = jwtAccessDeniedHandler;
+        this.jwtAuthenticationEntryPoint = jwtAuthenticationEntryPoint;
     }
 
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    /*@Bean
-    public UserDetailsManager users(DataSource dataSource) {
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("password")
-                .roles("USER")
-                .build();
-        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
-        users.createUser(user);
-        return users;
-    }*/
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/api/v1/**").permitAll()
+                .antMatchers("/api/v2/**").hasAuthority("SUPER_ADMIN")
+                .anyRequest().authenticated() //should be after all matchers
+
+                // disable Cross Site Request Forgery (for non-browser clients)
+                .and()
+                .csrf().disable()
+
+                // specify what can connect to our API
+                .cors() // allow OPTIONS request
+                .and()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // Spring Security, dont manage or create sessions (REST API)
+
+                .and()
+                .exceptionHandling().accessDeniedHandler(jwtAccessDeniedHandler)
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and()
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);  // filter that works for each request and sets up security context each time
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    /*
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("https://127.0.0.1:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET","PUT","POST","DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+    */
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManagerBean();
+    }
 
 }
