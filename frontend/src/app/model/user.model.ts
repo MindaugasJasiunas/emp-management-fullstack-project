260039cf801bf1a14a0f4ec81888de52670f7345
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-06-22 13:54:50 +0300

    User model updated. #25

--- frontend/src/app/model/user.model.ts
+++ frontend/src/app/model/user.model.ts
@@ -2,18 +2,19 @@ import { Role } from './role.model';
 
 export class User {
   constructor(
-    public publicId: string,
+    public publicId: string | null,
     public username: string,
     public email: string,
+    public password: string | null,
     public firstName: string,
     public lastName: string,
     public profileImageUrl: string,
     public dateOfBirth: string,
-    public age: number,
+    public age: number | null,
     public joinDate: string,
     public lastLoginDate: string,
     public active: boolean,
     public notLocked: boolean,
-    public roles: Role[]
+    public roles: Role[] | null
   ) {}
 }
