b3fbcae378723339a9ac37372ce522b48e03546d
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-06-22 11:05:43 +0300

    @auth0/angular-jwt dependency added, authentication service implemented #25

--- frontend/src/app/service/authentication.service.ts
+++ frontend/src/app/service/authentication.service.ts
@@ -8,11 +8,15 @@ import { environment } from 'src/environments/environment';
 import { Observable } from 'rxjs';
 import { User } from '../model/user.model';
 
+import { JwtHelperService } from '@auth0/angular-jwt';
+
 @Injectable({ providedIn: 'root' })
 export class AuthenticationService {
-  private host = environment.apiUrl;
   private hostLogin = environment.apiLoginUrl;
   private hostRegister = environment.apiRegisterUrl;
+  private token: string | null = null;
+  private loggedInUsername: string | null = null;
+  private jwtHelper = new JwtHelperService();
 
   constructor(private http: HttpClient) {}
 
@@ -38,5 +42,51 @@ export class AuthenticationService {
       this.hostRegister,
       registerRequest
     );
+  }
+
+  public logout(): void {
+    this.token = null;
+    this.loggedInUsername = null;
+
+    // remove items from localStorage - token, user information
+    localStorage.removeItem('user');
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  public saveToken(token: string): void {
+    this.token = token;
+    localStorage.setItem('token', token);
+  }
+
+  public addUserToLocalCache(user: User): void {
+    this.loggedInUsername = user.username;
+    localStorage.setItem('user', JSON.stringify(user));
+  }
+
+  public getUserFromLocalCache(): User {
+    return JSON.parse(localStorage.getItem('user')!);
+  }
+
+  public loadTokenFromLocalCache(): void {
+    this.token = localStorage.getItem('token')!;
+  }
+
+  public getTokenFromLocalCache(): string | null {
+    return this.token;
+  }
+
+  public isLoggedIn(): boolean {
+    this.loadTokenFromLocalCache();
+    if (this.token != null && this.token !== '') {
+      if (this.jwtHelper.decodeToken(this.token).sub != null || '') {
+        if (!this.jwtHelper.isTokenExpired(this.token)) {
+          this.loggedInUsername = this.jwtHelper.decodeToken(this.token).sub;
+          return true;
+        }
+      }
+    }
+    this.logout();
+    return false;
   }
 }
