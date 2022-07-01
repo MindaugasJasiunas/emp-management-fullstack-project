81911d07dabd0bbd60eb55fcfaf4717b77d08980
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-07-01 11:37:03 +0300

    app-routing updated - register endpoint added #33, guard added #30

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './guard/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RegisterComponent } from './register/register.component';
 import { UsersComponent } from './users/users.component';
+import { UserResolver } from './users/users.resolver';
 
 const routes: Routes = [
-  { path: ``, component: UsersComponent },
-  { path: `users`, component: UsersComponent },
-  { path: `profile`, component: ProfileComponent },
+  { path: `users`, component: UsersComponent, canActivate: [AuthGuard]/*, resolve: { users: UserResolver }*/ },
+  { path: `profile`, component: ProfileComponent, canActivate: [AuthGuard] },
   { path: `login`, component: LoginComponent },
+  { path: `register`, component: RegisterComponent },
+  { path: ``, redirectTo: '/users', pathMatch: 'full' },
 ];
 
 @NgModule({
