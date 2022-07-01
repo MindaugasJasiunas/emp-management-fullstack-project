93c470c1f01b66f2b842b257f379ce8518fcf6dd
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-07-01 11:33:20 +0300

    header updated #34

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -6,5 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
-  constructor() { }
+  constructor(private authenticationService: AuthenticationService) {}
+
+  isLoggedIn(): boolean{
+    return this.authenticationService.isLoggedIn();
+  }
 }
