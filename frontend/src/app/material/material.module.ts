11b79ee85c304f99275245e825ba3169df99cf6e
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-07-01 11:35:12 +0300

    material imports updated #35

--- frontend/src/app/material/material.module.ts
+++ frontend/src/app/material/material.module.ts
@@ -4,6 +4,14 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatNativeDateModule, MatOptionModule } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatPaginatorModule } from '@angular/material/paginator';
 
 const materialComponents = [
   MatButtonModule,
@@ -11,6 +19,15 @@ const materialComponents = [
   MatIconModule,
   MatCardModule,
   MatInputModule,
+  MatTableModule,
+  MatDialogModule,
+  MatCheckboxModule,
+  MatFormFieldModule,
+  MatOptionModule,
+  MatSelectModule,
+  MatDatepickerModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
 ];
 
 @NgModule({
