7a098f7f5e0d88cb1569e9482c4e9027e607f56c
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-07-04 10:53:15 +0300

    header updated - implemented logout button #34

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,18 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { AuthenticationService } from '../service/authentication.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
-  constructor(private authenticationService: AuthenticationService) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) {}
 
-  isLoggedIn(): boolean{
+  isLoggedIn(): boolean {
     return this.authenticationService.isLoggedIn();
   }
-  getUsername(): string | null{
+  getUsername(): string | null {
     return this.authenticationService.getUserFromLocalCache().username;
+  }
+
+  logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 }
