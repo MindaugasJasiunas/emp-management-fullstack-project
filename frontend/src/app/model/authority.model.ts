8c56cbef5ec648c751dc0154da5bae00a9c977e0
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-07-06 10:37:22 +0300

    added authorities to roles & updated profile component to show authorities by role. Closes #37

---
+++ frontend/src/app/model/authority.model.ts
@@ -0,0 +1,3 @@
+export class Authority {
+  constructor(public id: number, public permission: string){}
+}
--- frontend/src/app/model/role.model.ts
+++ frontend/src/app/model/role.model.ts
@@ -1,3 +1,5 @@
+import { Authority } from "./authority.model";
+
 export class Role {
-  constructor(public id: number, public roleName: string){}
+  constructor(public id: number, public roleName: string, public authorities?: Authority[]){}
 }
