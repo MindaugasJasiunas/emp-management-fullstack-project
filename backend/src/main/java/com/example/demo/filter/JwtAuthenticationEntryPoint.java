6c98dd6354be84ac752c152c7ee2028603034f43
Mindaugas Jasiunas <mmjasiunas@gmail.com>
2022-07-06 12:06:16 +0300

    Updated JWT Token Provider & Entry Point Error handler to handle error when JWT token expired #23

--- backend/src/main/java/com/example/demo/filter/JwtAuthenticationEntryPoint.java
+++ backend/src/main/java/com/example/demo/filter/JwtAuthenticationEntryPoint.java
@@ -1,6 +1,8 @@
 package com.example.demo.filter;
 
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.example.demo.HttpResponse;
+import com.example.demo.utility.JWTTokenProvider;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -15,11 +17,26 @@ import java.io.OutputStream;
 
 @Component
 public class JwtAuthenticationEntryPoint extends Http403ForbiddenEntryPoint {
+    private final JWTTokenProvider jwtTokenProvider;
+
+    public JwtAuthenticationEntryPoint(JWTTokenProvider jwtTokenProvider) {
+        this.jwtTokenProvider = jwtTokenProvider;
+    }
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException arg2) throws IOException {
         // generate custom response body
         HttpResponse customResponse = new HttpResponse(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN, HttpStatus.FORBIDDEN.getReasonPhrase(), "You need to login to access this page");
+        String token = request.getHeader("Authorization");
+        if(token != null && !token.trim().isEmpty()){
+            token = request.getHeader("Authorization").substring(7);
+        }
+
+        try{
+            jwtTokenProvider.isTokenExpired(token);
+        }catch (Exception e){
+            if(e instanceof TokenExpiredException) customResponse = new HttpResponse(HttpStatus.FORBIDDEN.value(), HttpStatus.FORBIDDEN, HttpStatus.FORBIDDEN.getReasonPhrase(), "JWT token expired");
+        }
 
         // send forbidden error
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
--- backend/src/main/java/com/example/demo/utility/JWTTokenProvider.java
+++ backend/src/main/java/com/example/demo/utility/JWTTokenProvider.java
@@ -6,6 +6,7 @@ import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTDecodeException;
 import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.auth0.jwt.exceptions.SignatureVerificationException;
+import com.auth0.jwt.exceptions.TokenExpiredException;
 import com.auth0.jwt.interfaces.Claim;
 import com.example.demo.domain.UserPrincipal;
 import org.springframework.boot.context.properties.ConfigurationProperties;
@@ -69,7 +70,7 @@ public class JWTTokenProvider {
         String username;
         try{
             username = getJWTVerifier().verify(token).getSubject();
-        }catch (JWTDecodeException | SignatureVerificationException e){
+        }catch (TokenExpiredException | JWTDecodeException | SignatureVerificationException e){
             return false;
         }
         return ((username != null) && (username.trim() != "") && (!isTokenExpired(token)));
@@ -79,7 +80,7 @@ public class JWTTokenProvider {
         return getJWTVerifier().verify(token).getSubject();
     }
 
-    private boolean isTokenExpired(String token){
+    public boolean isTokenExpired(String token){
         Date expiration = getJWTVerifier().verify(token).getExpiresAt();
         return expiration.before(new Date());
     }
